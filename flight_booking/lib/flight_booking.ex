80a6efbeef3905feb775c908b8773d476a54aea4
jaobehenrique <henriquecidoz@hotmail.com>
2022-01-04 22:29:20 -0300

    first commit: subindo projeto ao repositorio

---
+++ flight_booking/lib/flight_booking.ex
@@ -0,0 +1,18 @@
+defmodule FlightBooking do
+  @moduledoc """
+  Documentation for `FlightBooking`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlightBooking.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
