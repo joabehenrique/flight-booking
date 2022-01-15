77e72acaf98149839f60fc4f660892b996efa243
joabehenrique <henriquecidoz@hotmail.com>
2022-01-15 18:50:47 -0300

    test commit

--- flight_booking/lib/flight_booking.ex
+++ flight_booking/lib/flight_booking.ex
@@ -1,17 +1,4 @@
 defmodule FlightBooking do
-  @moduledoc """
-  Documentation for `FlightBooking`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FlightBooking.hello()
-      :world
-
-  """
   def hello do
     :world
   end
